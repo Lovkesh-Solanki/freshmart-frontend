cb1c5451778e2f74a141b92260d1fc39d5598409
Lovkesh Solanki <lovkesh4955@gmail.com>
2026-02-26 22:47:12 +0530

    Refactor routes to include Wishlist and cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-// ❌ DO NOT import BrowserRouter here!
-
-// Import pages
 import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
 import Products from './pages/Products';
 import ProductDetail from './pages/ProductDetail';
 import Cart from './pages/Cart';
@@ -12,6 +7,9 @@ import Checkout from './pages/Checkout';
 import Orders from './pages/Orders';
 import OrderDetail from './pages/OrderDetail';
 import Profile from './pages/Profile';
+import Wishlist from './pages/Wishlist';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import AdminDashboard from './pages/AdminDashboard';
 import AdminProducts from './pages/AdminProducts';
 import AdminCategories from './pages/AdminCategories';
@@ -20,89 +18,27 @@ import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Routes>  
+    <Routes>
       {/* Public Routes */}
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
       <Route path="/products" element={<Products />} />
       <Route path="/products/:id" element={<ProductDetail />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
 
-      {/* Protected Routes */}
-      <Route 
-        path="/cart" 
-        element={
-          <ProtectedRoute>
-            <Cart />
-          </ProtectedRoute>
-        } 
-      />
-      <Route 
-        path="/checkout" 
-        element={
-          <ProtectedRoute>
-            <Checkout />
-          </ProtectedRoute>
-        } 
-      />
-      <Route 
-        path="/orders" 
-        element={
-          <ProtectedRoute>
-            <Orders />
-          </ProtectedRoute>
-        } 
-      />
-      <Route 
-        path="/orders/:id" 
-        element={
-          <ProtectedRoute>
-            <OrderDetail />
-          </ProtectedRoute>
-        } 
-      />
-      <Route 
-        path="/profile" 
-        element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        } 
-      />
+      {/* Protected User Routes */}
+      <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
+      <Route path="/checkout" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
+      <Route path="/orders" element={<ProtectedRoute><Orders /></ProtectedRoute>} />
+      <Route path="/orders/:id" element={<ProtectedRoute><OrderDetail /></ProtectedRoute>} />
+      <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+      <Route path="/wishlist" element={<ProtectedRoute><Wishlist /></ProtectedRoute>} />
 
       {/* Admin Routes */}
-      <Route 
-        path="/admin" 
-        element={
-          <ProtectedRoute>
-            <AdminDashboard />
-          </ProtectedRoute>
-        } 
-      />
-      <Route 
-        path="/admin/products" 
-        element={
-          <ProtectedRoute>
-            <AdminProducts />
-          </ProtectedRoute>
-        } 
-      />
-      <Route 
-        path="/admin/categories" 
-        element={
-          <ProtectedRoute>
-            <AdminCategories />
-          </ProtectedRoute>
-        } 
-      />
-      <Route 
-        path="/admin/orders" 
-        element={
-          <ProtectedRoute>
-            <AdminOrders />
-          </ProtectedRoute>
-        } 
-      />
+      <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+      <Route path="/admin/products" element={<ProtectedRoute><AdminProducts /></ProtectedRoute>} />
+      <Route path="/admin/categories" element={<ProtectedRoute><AdminCategories /></ProtectedRoute>} />
+      <Route path="/admin/orders" element={<ProtectedRoute><AdminOrders /></ProtectedRoute>} />
     </Routes>
   );
 }
