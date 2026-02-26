04ac269e7adcce77f9f6e55bf0acc68b7094d99c
Lovkesh Solanki <lovkesh4955@gmail.com>
2026-02-26 13:38:42 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 API.interceptors.request.use((config) => {
@@ -24,3 +24,4 @@ API.interceptors.response.use(
 );
 
 export default API;
+
