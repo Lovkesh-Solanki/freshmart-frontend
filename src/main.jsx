702350c896c62b9b4a9443c7e9090ce2f1be1d2d
Lovkesh Solanki <lovkesh4955@gmail.com>
2026-07-20 11:56:28 +0530

    Create NotificationContext and update main.jsx with NotificationProvider

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import { WishlistProvider } from './context/WishlistContext';
+import { NotificationProvider } from './context/NotificationContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -13,7 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <AuthProvider>
         <CartProvider>
           <WishlistProvider>
-            <App />
+            <NotificationProvider>
+              <App />
+            </NotificationProvider>
           </WishlistProvider>
         </CartProvider>
       </AuthProvider>
